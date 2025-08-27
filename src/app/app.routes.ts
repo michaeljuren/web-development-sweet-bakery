48fed20cbb593a6af92ee39040021056a55b23f3
michaeljuren <michael16.uren@gmail.com>
2025-08-27 14:57:31 +0200

    Add configuration files and routing setup for application components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { OrderPageComponent } from './components/order-page/order-page.component';
+import { HomeComponent } from './components/home/home.component';
+import { BreadComponent } from './components/breads/bread.component';
+import { PastriesComponent } from './components/pastries/pastries.component';
+import { CakesComponent } from './components/cakes/cakes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'order', component: OrderPageComponent },
+  { path: 'breads', component: BreadComponent },
+  { path: 'pastries', component: PastriesComponent },
+  { path: 'cakes', component: CakesComponent }
+];
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideRouter, withViewTransitions, withRouterConfig, withInMemoryScrolling } from '@angular/router';
+import { routes } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled'
+      })
+    ),
+  ],
+})
   .catch((err) => console.error(err));
